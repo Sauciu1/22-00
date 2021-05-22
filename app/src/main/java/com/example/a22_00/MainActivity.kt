8c29e98029712ca441b4daa89ac569128426140b
Gintas <brolis.gintux@gmail.com>
2021-05-23 00:40:47 +0300

    db su listview surisau, bet po kolkas dizainas, tai vezys. (Jei kas netingi būt faina sutvarkyti)

--- app/src/main/java/com/example/a22_00/MainActivity.kt
+++ app/src/main/java/com/example/a22_00/MainActivity.kt
@@ -5,10 +5,12 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.graphics.*
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a22_00.DBHelper.DBHelper
 import com.example.a22_00.Model.MyListAdapter
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -52,24 +55,13 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        val db = DBHelper(this.applicationContext)
+        val data = db.getAllTimetables()
         // Čia reikia db data pversi į array ir tada galima sitas kodas veiks
-        val listView = findViewById<ListView>(R.id.listView)
-        val language =arrayOf<String>("C","C++","Java",".Net","Kotlin","Ruby","Rails","Python","Java Script","Php","Ajax","Perl","Hadoop")
-        val description = arrayOf<String>("C programming is considered as the base for other programming languages",
-                "C++ is an object-oriented programming language.",
-                "Java is a programming language and a platform.",
-                ".NET is a framework which is used to develop software applications.",
-                "Kotlin is a open-source programming language, used to develop Android apps and much more.",
-                "Ruby is an open-source and fully object-oriented programming language.",
-                "Ruby on Rails is a server-side web application development framework written in Ruby language.",
-                "Python is interpreted scripting  and object-oriented programming language.",
-                "JavaScript is an object-based scripting language.",
-                "PHP is an interpreted language, i.e., there is no need for compilation.",
-                "AJAX allows you to send and receive data asynchronously without reloading the web page.",
-                "Perl is a cross-platform environment used to create network and server-side applications.",
-                "Hadoop is an open source framework from Apache written in Java."  )
-
-        val myListAdapter = MyListAdapter(this,language,description)
+        val listView = findViewById<ListView>(R.id.listView)  //formos elementas kuriame sis sarasas yra sukuriamas
+        /*val names = data.mapNotNull{it.name}.toTypedArray()
+        val descriptions = data.mapNotNull { it.description+"\n${it.activities.size}" }.toTypedArray()*/
+        val myListAdapter = MyListAdapter(this,data.toTypedArray())
         listView.adapter = myListAdapter
 
         listView.setOnItemClickListener(){adapterView, view, position, id ->
--- app/src/main/java/com/example/a22_00/Model/MyListAdapter.kt
+++ app/src/main/java/com/example/a22_00/Model/MyListAdapter.kt
@@ -6,8 +6,8 @@ import android.view.ViewGroup
 import android.widget.*
 import com.example.a22_00.R
 
-class MyListAdapter(private val context: Activity, private val title: Array<String>, private val description: Array<String>)
-    : ArrayAdapter<String>(context, R.layout.row, title) {
+class MyListAdapter(private val context: Activity, private val timetables: Array<Timetable>)
+    : ArrayAdapter<String>(context, R.layout.row, timetables.mapNotNull { it.name }.toTypedArray()) {
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater = context.layoutInflater
@@ -15,9 +15,13 @@ class MyListAdapter(private val context: Activity, private val title: Array<Stri
 
         val titleText = rowView.findViewById(R.id.title) as TextView
         val subtitleText = rowView.findViewById(R.id.description) as TextView
+        val listView = rowView.findViewById(R.id.activitylist) as ListView
 
-        titleText.text = title[position]
-        subtitleText.text = description[position]
+        //val myListAdapter = MyListAdapter(this,data.toTypedArray())
+        listView.adapter = ActivityListAdapter(context, timetables[position].activities.toTypedArray())
+
+        titleText.text = timetables[position].name
+        subtitleText.text = timetables[position].description
 
         return rowView
     }
